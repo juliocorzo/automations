af2accc1e0834eb3260f8efc246d6bf10241fcc4
Julio Corzo <juliocorzo@me.com>
2024-01-22 12:41:56 -0500

    push fake metrics

--- src/pages/api/performance-metric/index.ts
+++ src/pages/api/performance-metric/index.ts
@@ -5,6 +5,8 @@ import type { AquasuiteResponse, AquasuiteFlatResponse } from './types';
 
 // const prisma = new PrismaClient();
 
+const GENERATE_FAKE_METRIC = true;
+
 export const generateMetric = (data: AquasuiteResponse): AquasuiteFlatResponse => {
   const metricsArray = data.d;
 
@@ -15,6 +17,28 @@ export const generateMetric = (data: AquasuiteResponse): AquasuiteFlatResponse =
   });
 
   return { ...metrics, timestamp: new Date(data.t) };
+};
+
+const generateFakeMetric = (): AquasuiteFlatResponse => {
+  const fakeMetric = {
+    liquid_in_temp: 0,
+    liquid_out_temp: 0,
+    front_mid_air_temp: 0,
+    back_top_air_temp: 0,
+    back_mid_air_temp: 0,
+    cpu_package_temp: 0,
+    gpu_core_temp: 0,
+    cooling_power: 0,
+    cpu_package_power: 0,
+    gpu_package_power: 0,
+    liquid_flow_rate: 0,
+    cpu_load: 0,
+    memory_load: 0,
+    gpu_load: 0,
+    timestamp: new Date('2024-01-15T06:36:46.889287Z'),
+  };
+
+  return fakeMetric;
 };
 
 export const generateDatabaseMetric = (data: AquasuiteFlatResponse): Omit<PerformanceMetric, 'id'> => {
@@ -45,6 +69,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const url = `https://aquasuite.aquacomputer.de/view/${AQUASUITE_KEY}`;
 
   try {
+    if (GENERATE_FAKE_METRIC) {
+      res.status(200).json(generateFakeMetric());
+    }
     const response = await fetch(url);
     const body: AquasuiteResponse = await response.json();
     const metric = generateMetric(body);
